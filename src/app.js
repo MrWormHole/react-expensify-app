f4f2de41b5ac75015477fad3f9d9ef4f5b05a92c
Talha Altınel <talhaaltinel@hotmail.com>
2019-12-05 15:24:49 +0300

    fixed dependency error that is caused by small typo

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import  "./styles/styles.scss";
 const store = configureStore();
 
 store.dispatch(addExpense( {description: "water bill", amount: 4500} ));
-store.dispatch(addExpense( {description: "gas bill", createdAt: 1000} ));
+store.dispatch(addExpense( {description: "gas bill", createdAt: 1000} ));//
 store.dispatch(addExpense( {description: "rent", amount: 109500} ));
 store.dispatch(setTextFilter('water bill'));
 
@@ -26,8 +26,8 @@ const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
 const jsx = (
-    <Provider>
-        <AppRouter store={store}/>
+    <Provider store={store}>
+        <AppRouter/>
     </Provider>
 );
 
