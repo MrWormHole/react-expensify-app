61b47c96a22ad0a5afe5784dbdd16902db8c5f48
Talha Altınel <talhaaltinel@hotmail.com>
2019-12-09 08:56:19 +0300

    small change

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ import  "./styles/styles.scss";
 
 const store = configureStore();
 
+//TEST IF THINGS ARE WORKING
+//THEN SOLVE THE PULLING PROBLEM AND PUSH
+
 store.dispatch(addExpense( {description: "water bill", amount: 4500} ));
 store.dispatch(addExpense( {description: "gas bill", createdAt: 1000} ));//
 store.dispatch(addExpense( {description: "rent", amount: 109500} ));
