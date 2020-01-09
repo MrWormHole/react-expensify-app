b00cb2793715f1801ce26a5b9be133fe752fa5c6
MrWormHole <talhaaltinel@hotmail.com>
2020-01-09 00:57:58 +0000

    expense summary component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ExpenseList from "./ExpenseList";
 import ExpenseListFilters from "./ExpenseListFilters";
+import ExpenseSummary from "./ExpenseSummary";
 
 const ExpenseDashboardPage = () => {
     return  (
         <div>
+		<ExpenseSummary />
             <ExpenseListFilters />
             <ExpenseList />
         </div>
